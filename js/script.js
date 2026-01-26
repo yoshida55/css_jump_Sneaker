889406d49f75d4b8fef27b82a4788848159816f2
github user name <sensha5172@gmail.com>
2026-01-27 05:30:12 +0900

    あああ

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 // ハンバーガーメニュークリック時にクラス（open）を付け外しする
 const hamburgerBtn = document.getElementById("hamburger-btn");
 const mask = document.getElementById("mask");
+const gNav = document.getElementById("g-nav");
 
 hamburgerBtn.addEventListener("click", function () {
   // オーバーレイを表示・非表示にする(openにする・外す)
   mask.classList.toggle("open");
   // ボタン自身に .open を付けて「×」にする
   hamburgerBtn.classList.toggle("open");
+  // ナビゲーションメニューを表示・非表示にする
+  gNav.classList.toggle("open");
 });
