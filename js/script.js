a85208979e318b75575426bf929cf188333d1ecd
github user name <sensha5172@gmail.com>
2026-01-28 08:29:41 +0900

    あああ

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // ハンバーガーメニュークリック時にクラス（open）を付け外しする
-const hamburgerBtn = document.getElementById("hamburger-btn");
+const hamburgerBtn = document.getElementById("hamburger_btn");
 const mask = document.getElementById("mask");
-const gNav = document.getElementById("g-nav");
+const gNav = document.getElementById("g_nav");
 
 hamburgerBtn.addEventListener("click", function () {
   // オーバーレイを表示・非表示にする(openにする・外す)
@@ -32,5 +32,26 @@ $(function () {
         },
       },
     ],
+  });
+});
+
+/*=================================================
+画像のフェードイン
+===================================================*/
+$(window).scroll(function () {
+  // feature_item の画像すべてに対して処理
+  $(".feature_item img").each(function () {
+    // スクロールした距離
+    let scroll = $(window).scrollTop();
+    // この画像までの距離
+    let target = $(this).offset().top;
+    // 画面の高さ
+    let windowHeight = $(window).height();
+
+    // 画像が画面下から200px入ったら表示
+    if (scroll > target - windowHeight + 200) {
+      $(this).css("opacity", "1");
+      $(this).css("transform", "translateY(0)");
+    }
   });
 });
